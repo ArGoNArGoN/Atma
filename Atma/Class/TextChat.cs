e45defdcc2026f18df7dbb622987c9ee37a04f83
Aвфцвф <70173258+ArGoNArGoN@users.noreply.github.com>
2020-10-23 19:04:53 +0300

    Add TextChat

--- Atma/Class/TextChat.cs
+++ Atma/Class/TextChat.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace Atma.Class
 {
-    class TextChat : IChat
-    {
+	internal sealed class TextChat : IChat
+	{
+		private Int32 id;
+		private String name;
+		private String info;
+		private Int32? maxCountUser;
+		private List<ServerUser> serverUsers = new List<ServerUser>();
+
+        public TextChat(int id, string name, string info, int? maxCountUser)
+        {
+			try
+			{
+				Id = id;
+				Name = name;
+				Info = info;
+				MaxCountUser = maxCountUser;
+			}
+            catch { throw; }
+        }
+
+        public List<ServerUser> ServerUsers 
+		{
+			get => serverUsers;
+			set => serverUsers = value ??
+					throw new ArgumentNullException("value is null");
+		}
+
+		public Int32 Id
+        {
+			get => id;
+            set
+            {
+				if (value < 0) throw new ArgumentException("value < 0", "value");
+				id = value;
+			}
+		}
+
+		public String Name
+        {
+			get => name;
+            set
+            {
+				if (String.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException("value is null");
+				if (value.Length > 50)
+					throw new ArgumentException("value.Length > 50", "value");
+				name = value;
+			}
+		}
+
+		public String Info
+        {
+			get => info;
+            set
+            {
+				if (value == null)
+					throw new ArgumentNullException("value is null", "value");
+				if (value.Length > 500)
+					throw new ArgumentException("value.Length > 500", "value");
+				info = value;
+			}
+		}
+
+		public Int32? MaxCountUser
+        {
+			get => maxCountUser;
+			set
+            {
+				if (value < 2)
+					throw new ArgumentException("value < 2", "value");
+				maxCountUser = value;
+			}
+		}
     }
 }
