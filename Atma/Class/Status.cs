957af8a0bd64539b5f206bda37f4d96439e00b66
Aвфцвф <70173258+ArGoNArGoN@users.noreply.github.com>
2020-10-23 14:27:19 +0300

    dawd

---
+++ Atma/Class/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atma.Class
+{
+    internal enum Status : Int32
+    {
+        OutOfPlace,
+        Online,
+        Offline
+    }
+}
